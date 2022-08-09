abe4e7f70e02dfb6bef015fad065c605c674782a
DLP-Sekara <dslahirupiyumal99@gmail.com>
2022-08-09 17:26:44 +0530

    not found page completed

--- src/App.js
+++ src/App.js
@@ -1,9 +1,21 @@
 import logo from './logo.svg';
 import './App.css';
 import Login from "./pages/login/login";
+import NotFound from "./pages/notFound/NotFound";
+import {Route, Routes} from "react-router-dom";
 function App() {
   return (
-  <Login/>
+
+      <Routes>
+        <Route exact path="/"element={<Login/>}/>
+       {/* <Route  path='dashboard' element={<Dashboard/>}/>
+        <Route path="cart"element={<Cart/>}/>
+        <Route path="product"element={<Product/>}/>
+        <Route path="user"element={<User/>}/>*/}
+          <Route path="*"element={<NotFound/>}/>
+       </Routes>
+
+
   );
 }
 
