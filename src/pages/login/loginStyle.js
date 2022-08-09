9fb5b24f4295ed21ef3413c2511b41a4055471df
DLP-Sekara <dslahirupiyumal99@gmail.com>
2022-08-09 17:12:17 +0530

    login page ui completed

---
+++ src/pages/login/loginStyle.js
@@ -0,0 +1,46 @@
+export const styleSheet = {
+    container: {
+        display: 'flex',
+        flexWrap: 'wrap',
+        flexDirection: 'column',
+        height: '100vh',
+        width: '100vw',
+        alignItems: 'center',
+        justifyContent: 'center'
+
+    },
+    login__cover: {
+        display: 'flex',
+        flexWrap: 'wrap',
+        flexDirection: 'column',
+        height: '70%',
+        width: '30%',
+        alignItems: 'center',
+        justifyContent: 'center',
+        background : '#ad9595',
+        color : 'white',
+        fontWeight :'bold'
+    },
+    title__container: {
+        display: 'flex',
+        alignItems: 'center',
+        height: '20%',
+    },
+    form__container: {
+        display: 'flex',
+        flexDirection: 'column',
+        height: '50%',
+        width: '100%',
+        alignItems: 'center',
+        justifyContent: 'space-around',
+    },
+    btn__container: {
+        height: '20%',
+    },
+    form__username: {
+        background: 'white'
+    },
+    form__password: {
+        background: 'white'
+    }
+}
